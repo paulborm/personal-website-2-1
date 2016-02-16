401efb995efe3328d47f4f1932dee93c82048e08
Paul Borm <zenbormi@gmx.de>
2016-02-16 16:43:50 +0100

    removed all from main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,3 @@
 $(document).ready(function () {
 
-    $('.nav-toggle').on('click', function () {
-        $(this).toggleClass('nav-toggle--open');
-        $(".nav-fullscreen").toggleClass("nav-fullscreen--open");
-    });
-
 });
